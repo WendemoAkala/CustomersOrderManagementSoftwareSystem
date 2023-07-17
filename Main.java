105b2550fffb4795f8f55698bbf52ab0d49304b6
WendemoAkala <101048086+WendemoAkala@users.noreply.github.com>
2023-07-17 22:17:27 +0300

    new main

--- Main.java
+++ Main.java
@@ -2,48 +2,47 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-// Create order items
         OrderItem item1 = new OrderItem(1, "Item 1", 10.0);
         OrderItem item2 = new OrderItem(2, "Item 2", 20.0);
-        OrderItem item3 = new OrderItem(2, "Item 2", 20.0);
+        OrderItem item3 = new OrderItem(2, "Item 3", 30.0);
 
-// Create a customer
-        Customer customer = new Customer(1, "John", "Doe", "john@example.com", "123 Street", CustomerType.VIP, 0.1);
-        Customer customer2 = new Customer(1, "Doe", "John", "Doe@example.com", "456 Street", CustomerType.VIP, 0.5);
+        Customer customer1 = new Customer(1, "Ben", "Meir", "ben@gmail.com", "123 Street", CustomerType.VIP, 0.1);
+        Customer customer2 = new Customer(2, "Amir", "Yosef", "amir@gmail.com", "456 Street", CustomerType.REGULAR, 0.5);
+        Customer customer3 = new Customer(2, "Yossi", "Akala", "yossi@gmail.com", "789 Street", CustomerType.REGULAR, 0.5);
 
-// Create an order and add items
         ArrayList<OrderItem> items = new ArrayList<>();
         items.add(item1);
         items.add(item2);
         items.add(item3);
-        Order order = new Order(2, "Order 1", "123 Street", items, customer, PaymentType.OTHER, "2023-07-17");
+        Order order = new Order(1, "Order 1", "123 Street", items, customer1, PaymentType.CASH, "2023-07-17");
+        Order order2 = new Order(2, "Order 1", "456 Street", items, customer2, PaymentType.CREDIT_CARD, "2023-07-17");
+        Order order3 = new Order(3, "Order 1", "789 Street", items, customer3, PaymentType.OTHER, "2023-07-17");
 
-// Add the order items to the customer's favorite items list
         for (OrderItem item : items) {
-            customer.addFavoriteItem(items);
-            }
+            customer1.addFavoriteItem(items);
+            customer2.addFavoriteItem(items);
+            customer3.addFavoriteItem(items);
+        }
 
 
-// Take a gift and open it
+
         CustomerGift gift = new CustomerGift();
-        customer.takeGift(gift);
-        customer.openGift();
-        customer.removeFavoriteItems(customer.getFavoriteItems());
+        customer1.takeGift(gift);
+        customer1.openGift();
         customer2.takeGift(gift);
         customer2.openGift();
 
-// Retrieve order details
-        System.out.println("Order ID: " + order.getId());
-        System.out.println("Order Name: " + order.getName());
-        System.out.println("Delivery Address: " + order.getDeliveryAddress());
+        System.out.println("Order ID: " + order2.getId());
+        System.out.println("Order Name: " + order2.getName());
+        System.out.println("Delivery Address: " + order2.getDeliveryAddress());
         System.out.println("Items: ");
-        for (OrderItem item : order.getItems()) {
+        for (OrderItem item : order2.getItems()) {
             System.out.println("  - " + item.getItemName() + ": $" + item.getItemPrice());
         }
-        System.out.println("Customer: " + order.getCustomer().getFirstName() + " " + order.getCustomer().getLastName());
-        System.out.println("Total Price: $" + order.getTotalPrice());
-        System.out.println("Payment Type: " + order.getPaymentType());
-        System.out.println("Order Date: " + order.getOrderDate());
+        System.out.println("Customer: " + order2.getCustomer().getFirstName() + " " + order2.getCustomer().getLastName());
+        System.out.println("Total Price: $" + order2.getTotalPrice());
+        System.out.println("Payment Type: " + order2.getPaymentType());
+        System.out.println("Order Date: " + order2.getOrderDate());
 
         }
     }
